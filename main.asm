05c22c129f36ec2e755fc080ea6889ce3486669e
Luciano Martorella <mad_lux_it@yahoo.it>
2022-01-30 10:36:11 +0100

    - Test common mem bank

--- main.asm
+++ main.asm
@@ -13,18 +13,43 @@
 boot:
     ; Disable interrupts
     di
-    ld sp, stack_top
-    ld a,12
-    push af
-    pop af
 
-main_loop:
-    ; Alternatively wait on vertical interrupt
+    ; Before using the common bank, test it
+    ld de, common_bank_bottom
+    ld a, $55
+    ld (de), a
+    inc de
+    ld a, $aa
+    ld (de), a
+    inc de
+
+    ld hl, common_bank_bottom
+    ld bc, common_bank_size
+    ldir
+
+    ; Test back
+    ld hl, common_bank_bottom
+    ld bc, common_bank_size
+
+loop1:
+    ld a, $55
+    cpi
+    jnz error
+    ld a, $aa
+    cpi
+    jnz error
+    ld a, b
+    or c
+    jnz loop1
+
     halt
 
-    jr main_loop
+error:
+    halt
 
 
+common_bank_bottom: equ $4000
+common_bank_size equ $2000
 ;===========================================================================
 ; Stack.
 ;===========================================================================
